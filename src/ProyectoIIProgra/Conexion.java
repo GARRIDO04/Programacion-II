faf8aecbefa2bcbd9bdfb04d8d3beb918e660b2a
Luis <Luis@192.168.0.246>
2026-06-08 23:16:00 -0600

    Actualizacion mensajes estado

--- src/ProyectoIIProgra/Conexion.java
+++ src/ProyectoIIProgra/Conexion.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Conexion {
-    private static final String URL = "jdbc:mysql://localhost:3306/InventarioHardwareSoftware";
+    private static final String URL = "jdbc:mysql://localhost:3306/inventariohardwaresoftware";
     private static final String USER = "root";
     private static final String PASSWORD = ""; 
 
@@ -13,6 +13,7 @@ public class Conexion {
         Connection con = null;
         try {
             con = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Conexion existosa");
         } catch (SQLException e) {
             System.err.println("Error de conexión: " + e.getMessage());
         }
